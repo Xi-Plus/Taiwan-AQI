5fa369015d1204c6ad386f0e8248624dd28e02c1
xiplus <huangxuanyuxiplus@gmail.com>
2016-08-24 18:52:10 +0800

    first commit

---
+++ config/config.example.php
@@ -0,0 +1,11 @@
+<?php
+$cfg["PSI_over"] = 51;
+$cfg["PM25_over"] = 4;
+
+$config['page_id'] = 'page_id';
+$config['verify_token'] = 'verify_token';
+
+$config['app_id'] = 'app_id';
+$config['app_secret'] = 'app_secret';
+$config['page_token'] = 'access_token';
+?>
