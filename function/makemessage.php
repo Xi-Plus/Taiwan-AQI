8e92890b0c98cdab4a168ca6408b46e2f31a3c95
xiplus <huangxuanyuxiplus@gmail.com>
2017-03-11 08:36:43 +0800

    time diff

--- function/makemessage.php
+++ function/makemessage.php
@@ -2,7 +2,7 @@
 function TimediffFormat($time) {
 	if ($time<60) return $time."秒";
 	if ($time<60*50) return round($time/60)."分";
-	if ($time<60*60*24) return round($time/(60*60))."小時";
+	if ($time<60*60*23.5) return round($time/(60*60))."小時";
 	return round($time/(60*60*24))."天";
 }
 function MakeMessage($force=false, $tmid=null) {
