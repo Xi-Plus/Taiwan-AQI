6ebb5e88a9b1510f093ac6100bf9090da16cb602
xiplus <huangxuanyuxiplus@gmail.com>
2018-01-22 09:23:46 +0800

    remove command prefix

--- follow.php
+++ follow.php
@@ -146,7 +146,7 @@ foreach ($row as $data) {
 					}
 					$city = next($D["city"]);
 				}
-				$msg .= "\n輸入 /add 接收測站通知";
+				$msg .= "\n輸入 add 接收測站通知";
 				SendMessage($tmid, $msg);
 				continue;
 			}
@@ -155,22 +155,15 @@ foreach ($row as $data) {
 				continue;
 			}
 			$msg = $messaging['message']['text'];
-			if ($msg[0] !== "/") {
-				SendMessage($tmid, "無法辨識的訊息\n".
-					"本粉專由機器人自動運作\n".
-					"啟用訊息通知輸入 /add\n".
-					"顯示所有命令輸入 /help");
-				continue;
-			}
 			$msg = str_replace("\n", " ", $msg);
 			$msg = preg_replace("/\s+/", " ", $msg);
 			$cmd = explode(" ", $msg);
 			switch ($cmd[0]) {
-				case '/add':
+				case 'add':
 					if (!isset($cmd[1])) {
-						$msg = "輸入 /add [區域] 顯示此區域所有的測站"."\n\n".
+						$msg = "輸入 add [區域] 顯示此區域所有的測站"."\n\n".
 							"可用的區域有：".implode("、", $D["arealist"])."\n\n".
-							"範例： /add ".$D["arealist"][0];
+							"範例： add ".$D["arealist"][0];
 						SendMessage($tmid, $msg);
 						break;
 					}
@@ -199,9 +192,9 @@ foreach ($row as $data) {
 						break;
 					}
 					if (in_array($city, $D["arealist"])) {
-						SendMessage($tmid, "輸入 /add [測站] 接收此測站通知\n".
+						SendMessage($tmid, "輸入 add [測站] 接收此測站通知\n".
 							"可用的測站有：".implode("、", $D["citylist"][$city])."\n\n".
-							"範例: /add ".$D["citylist"][$city][0]);
+							"範例: add ".$D["citylist"][$city][0]);
 					} else if (isset($D["city"][$city])) {
 						$user = getuserlist($tmid);
 						if (!isset($user[$city])) {
@@ -215,19 +208,19 @@ foreach ($row as $data) {
 							SendMessage($tmid, "已開始接收".$city."測站的通知，當AQI達到".$level."且變化達到".$diff."時會通知\n目前的AQI是 ".$D["city"][$city]["AQI"]);
 						} else {
 							SendMessage($tmid, $city." 測站已經接收過了\n".
-								"要修改請使用 /level 和 /diff\n".
-								"要刪除請使用 /del");
+								"要修改請使用 level 和 diff\n".
+								"要刪除請使用 del");
 						}
 					} else {
 						$msg = "找不到此區域或測站"."\n".
-							"輸入 /add [區域] 顯示此區域所有的測站"."\n\n".
+							"輸入 add [區域] 顯示此區域所有的測站"."\n\n".
 							"可用的區域有：".implode("、", $D["arealist"])."\n\n".
-							"範例： /add ".$D["arealist"][0];
+							"範例： add ".$D["arealist"][0];
 						SendMessage($tmid, $msg);
 					}
 					break;
 
-				case '/level':
+				case 'level':
 					if (!isset($cmd[1])) {
 						SendMessage($tmid, "參數不足\n".
 							"必須給出一個或兩個參數");
@@ -284,7 +277,7 @@ foreach ($row as $data) {
 					}
 					break;
 
-				case '/diff':
+				case 'diff':
 					if (!isset($cmd[1])) {
 						SendMessage($tmid, "參數不足\n".
 							"必須給出一個或兩個參數");
@@ -341,7 +334,7 @@ foreach ($row as $data) {
 					}
 					break;
 				
-				case '/del':
+				case 'del':
 					$sth = $G["db"]->prepare("SELECT * FROM `{$C['DBTBprefix']}follow` WHERE `tmid` = :tmid");
 					$sth->bindValue(":tmid", $tmid);
 					$res = $sth->execute();
@@ -351,13 +344,13 @@ foreach ($row as $data) {
 						$follow []= $temp["city"];
 					}
 					if (!isset($cmd[1])) {
-						$msg = "輸入 /del [測站] 取消接收此測站通知"."\n\n";
+						$msg = "輸入 del [測站] 取消接收此測站通知"."\n\n";
 						if (count($follow) == 0) {
 							$msg .= "沒有接收任何測站\n".
-								"輸入 /add [測站] 開始接收測站通知";
+								"輸入 add [測站] 開始接收測站通知";
 						} else {
 							$msg .= "接收的測站有：".implode("、", $follow)."\n\n".
-								"範例： /del ".$follow[0];
+								"範例： del ".$follow[0];
 						}
 						SendMessage($tmid, $msg);
 						break;
@@ -376,7 +369,7 @@ foreach ($row as $data) {
 					}
 					break;
 
-				case '/list':
+				case 'list':
 					$sth = $G["db"]->prepare("SELECT * FROM `{$C['DBTBprefix']}follow` WHERE `tmid` = :tmid");
 					$sth->bindValue(":tmid", $tmid);
 					$res = $sth->execute();
@@ -384,69 +377,69 @@ foreach ($row as $data) {
 
 					if (count($row) == 0) {
 						SendMessage($tmid, "沒有接收任何測站\n".
-							"輸入 /add [測站] 開始接收測站通知");
+							"輸入 add [測站] 開始接收測站通知");
 					} else {
 						$msg = "已接收以下測站"."\n";
 						foreach ($row as $follow) {
 							$msg .= $follow["city"]." 門檻".$follow["level"]." 變化".$follow["diff"]."\n";
 						}
 						$msg .= "\n".
-							"/level 修改門檻值\n".
-							"/diff 修改變化值\n".
-							"/del 停止測站通知";
+							"level 修改門檻值\n".
+							"diff 修改變化值\n".
+							"del 停止測站通知";
 						SendMessage($tmid, $msg);
 					}
 					break;
 				
-				case '/show':
-					require(__DIR__.'/function/level.php');
+				case 'show':
+					require(__DIR__.'/functionlevel.php');
 					require(__DIR__.'/function/makemessage.php');
 					if (!makemessage(true, $tmid)) {
 						SendMessage($tmid, "沒有接收任何測站\n".
-							"輸入 /add [測站] 開始接收測站通知");
+							"輸入 add [測站] 開始接收測站通知");
 					}
 					break;
 				
-				case '/help':
+				case 'help':
 					if (isset($cmd[2])) {
 						$msg = "參數過多\n".
 							"必須給出一個參數為指令的名稱";
 					} else if (isset($cmd[1])) {
 						switch ($cmd[1]) {
 							case 'add':
-								$msg = "/add 顯示所有區域\n".
-									"/add [區域] 顯示此區域所有的測站\n".
-									"/add [測站] 接收測站通知\n".
-									"/add [測站] [門檻] 接收並自訂通知門檻值\n".
-									"/add [測站] [門檻] [變化] 接收並自訂通知門檻和變化值";
+								$msg = "add 顯示所有區域\n".
+									"add [區域] 顯示此區域所有的測站\n".
+									"add [測站] 接收測站通知\n".
+									"add [測站] [門檻] 接收並自訂通知門檻值\n".
+									"add [測站] [門檻] [變化] 接收並自訂通知門檻和變化值";
 								break;
 							
 							case 'level':
-								$msg = "/level [測站] [門檻] 將此測站門檻值改為自訂值\n".
-									 "/level [測站] 將此測站門檻值改為預設值{$C["AQIover"]}\n".
-									 "/level [門檻] 將所有測站門檻值改為自訂值\n";
+								$msg = "level [測站] [門檻] 將此測站門檻值改為自訂值\n".
+									 "level [測站] 將此測站門檻值改為預設值{$C["AQIover"]}\n".
+									 "level [門檻] 將所有測站門檻值改為自訂值\n";
 								break;
 							
 							case 'diff':
-								$msg = "/diff [測站] [變化] 將此測站變化值改為自訂值\n".
-									 "/diff [測站] 將此測站變化值改為預設值{$C["AQIdiff"]}\n".
-									 "/diff [變化] 將所有測站變化值改為自訂值\n";
+								$msg = "diff [測站] [變化] 將此測站變化值改為自訂值\n".
+									 "diff [測站] 將此測站變化值改為預設值{$C["AQIdiff"]}\n".
+									 "diff [變化] 將所有測站變化值改為自訂值\n";
 								break;
 							
 							case 'del':
-								$msg = "/del [測站] 停止此測站通知";
+								$msg = "del [測站] 停止此測站通知";
 								break;
 							
 							case 'list':
-								$msg = "/list 列出已接收通知的測站";
+								$msg = "list 列出已接收通知的測站";
 								break;
 							
 							case 'show':
-								$msg = "/show 列出已接收通知測站的資料";
+								$msg = "show 列出已接收通知測站的資料";
 								break;
 							
 							case 'help':
-								$msg = "/help 顯示所有命令";
+								$msg = "help 顯示所有命令";
 								break;
 							
 							default:
@@ -455,21 +448,21 @@ foreach ($row as $data) {
 						}
 					} else {
 						$msg = "可用命令\n".
-						"/add 接收測站通知\n".
-						"/level 修改測站門檻值\n".
-						"/diff 修改測站變化值\n".
-						"/del 停止測站通知\n".
-						"/list 列出已接收通知的測站\n".
-						"/show 列出已接收通知測站的資料\n".
-						"/help 顯示所有命令\n\n".
-						"/help [命令] 顯示命令的詳細用法";
+						"add 接收測站通知\n".
+						"level 修改測站門檻值\n".
+						"diff 修改測站變化值\n".
+						"del 停止測站通知\n".
+						"list 列出已接收通知的測站\n".
+						"show 列出已接收通知測站的資料\n".
+						"help 顯示所有命令\n\n".
+						"help [命令] 顯示命令的詳細用法";
 					}
 					SendMessage($tmid, $msg);
 					break;
 				
 				default:
 					SendMessage($tmid, "無法辨識命令\n".
-						"輸入 /help 取得可用命令");
+						"輸入 help 取得可用命令");
 					break;
 			}
 		}
